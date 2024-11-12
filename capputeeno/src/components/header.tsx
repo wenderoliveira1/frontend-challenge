305505153e5df1a8990ed935c3029cbc296f165a
wenderoliveira1 <wenderemanoel.021@gmail.com>
2024-11-12 10:07:44 -0300

    changes

---
+++ capputeeno/src/components/header.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { Saira_Stencil_One } from "next/font/google";
+import { styled } from "styled-components";
+
+const sairaStencil = Saira_Stencil_One ({
+  weight: ['400'],
+  subsets: ['latin']
+})
+
+interface HeaderProps{
+
+}
+
+const TagHeader = styled.header`
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 20px 160px;
+
+`
+
+const Logo = styled.a`
+color: var(--logo-color);
+wont-weith: 400px;
+font-size: 40px;
+line-height: 150%;
+`
+
+export function Header(props: HeaderProps) {
+    <TagHeader>
+        <Logo className={sairaStencil.className}>Capputeeno</Logo>
+        <div></div>
+    </TagHeader>
+}
